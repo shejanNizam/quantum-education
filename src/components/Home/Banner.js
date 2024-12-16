029b06c653741adcbb06ab851d3acb538e2f2f34
nizam <nizam.shejan11@gmail.com>
2024-12-16 18:54:50 +0600

    banner added

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import BANNER_ING from "../../assets/banner_img1.webp";
+
 export default function Banner() {
   return (
     <>
-      <h3 className=" text-primary ">Banner</h3>
+      <div className="container flex flex-col-reverse md:flex-row lg:flex-row p-4 space-y-4 md:space-y-0 md:space-x-4">
+        <div className="flex-1">
+          <h1 className="text-8xl text-primary font-bold mt-8 mb-20">
+            Welcome To Quantum
+          </h1>
+          <p className="mb-12">
+            Discover an affordable, fast-track path to professional success at
+            our accelerated higher-learning institution.
+          </p>
+          <p>
+            Experience the fusion of academia and industry firsthand. Secure
+            your spot on our waiting list for the Summer Semester of 2024.
+          </p>
+          <div className="flex items-center gap-4 my-8">
+            <button className="customButton">Apply Today</button>
+            <button className="customButton">Learn More -- </button>
+          </div>
+        </div>
+        <div className="flex-1 lg:relative">
+          <Image src={BANNER_ING} alt="banner_img" fill={true} />
+        </div>
+      </div>
     </>
   );
 }
