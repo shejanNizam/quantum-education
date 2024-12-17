e37958d0e19ac6cea3a5dba0c10d081857878b1b
nizam <nizam.shejan11@gmail.com>
2024-12-17 16:03:32 +0600

    carousel add

--- src/components/Home/AlumniWork.js
+++ src/components/Home/AlumniWork.js
@@ -1,7 +1,41 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 export default function AlumniWork() {
   return (
-    <>
-      <h3>AlumniWork</h3>
-    </>
+    <div className="my-20">
+      <h2 className="text-6xl text-center font-bold mb-12">
+        {" "}
+        Where Our Alumni Work{" "}
+      </h2>
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full max-w-sm mx-auto"
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-3xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
 }
--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,7 +1,81 @@
+"use client";
+
+import Image from "next/image";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import image1 from "../../assets/testimonial/testimonial_image_1.png";
+import image2 from "../../assets/testimonial/testimonial_image_2.png";
+import image3 from "../../assets/testimonial/testimonial_image_3.png";
+import image4 from "../../assets/testimonial/testimonial_image_4.png";
+
 export default function Testimonials() {
+  const data = [
+    {
+      _id: 1,
+      image: image1,
+      name: "STUDENT NAME",
+      description:
+        "I would without a doubt recommend Quantum Institute of Science and Technology to anyone looking for a career change or wanting to try something different. My background is construction, and I heard about BPT through a well known family friend. Bright Path is much more different than your traditional college, in the sense that a college will make you pay such high tuition fees for only a small fraction of what someone needs to know going into IT. I have been in the course for a couple months and I can easily say that I've learned so much in the little time I've been here.",
+    },
+    {
+      _id: 2,
+      image: image2,
+      name: "STUDENT NAME",
+      description:
+        "I would without a doubt recommend Quantum Institute of Science and Technology to anyone looking for a career change or wanting to try something different. My background is construction, and I heard about BPT through a well known family friend. Bright Path is much more different than your traditional college, in the sense that a college will make you pay such high tuition fees for only a small fraction of what someone needs to know going into IT. I have been in the course for a couple months and I can easily say that I've learned so much in the little time I've been here.",
+    },
+    {
+      _id: 3,
+      image: image3,
+      name: "STUDENT NAME",
+      description:
+        "I would without a doubt recommend Quantum Institute of Science and Technology to anyone looking for a career change or wanting to try something different. My background is construction, and I heard about BPT through a well known family friend. Bright Path is much more different than your traditional college, in the sense that a college will make you pay such high tuition fees for only a small fraction of what someone needs to know going into IT. I have been in the course for a couple months and I can easily say that I've learned so much in the little time I've been here.",
+    },
+    {
+      _id: 4,
+      image: image4,
+      name: "STUDENT NAME",
+      description:
+        "I would without a doubt recommend Quantum Institute of Science and Technology to anyone looking for a career change or wanting to try something different. My background is construction, and I heard about BPT through a well known family friend. Bright Path is much more different than your traditional college, in the sense that a college will make you pay such high tuition fees for only a small fraction of what someone needs to know going into IT. I have been in the course for a couple months and I can easily say that I've learned so much in the little time I've been here.",
+    },
+  ];
+
   return (
-    <>
-      <h3>Testimonials</h3>
-    </>
+    <section className="container py-16">
+      <h2 className="text-6xl text-center mb-12 font-bold">
+        Authentic Student Testimonials
+      </h2>
+      <Swiper
+        navigation
+        pagination={{ type: "bullets", clickable: true }}
+        autoplay={true}
+        loop={true}
+        modules={[Autoplay, Navigation, Pagination]}
+        className="relative"
+      >
+        {data.map(({ _id, image, name, description }) => (
+          <SwiperSlide
+            key={_id}
+            className="bg-white flex justify-center items-center p-20"
+          >
+            <div className="flex flex-col items-center text-center ">
+              <Image
+                className="rounded-full mb-6"
+                width={100}
+                height={100}
+                src={image}
+                alt={`${name} testimonial`}
+                quality={100}
+              />
+              <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
+              <p className="text-gray-600 mt-4 px-2">{description}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 }
