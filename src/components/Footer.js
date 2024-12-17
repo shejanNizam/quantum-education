7f8ac094f9df2136a7e66ad3e3f1ab68cd248d4a
nizam <nizam.shejan11@gmail.com>
2024-12-17 10:52:11 +0600

    banner refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="bg-cover bg-center relative">
-      <div className="container grid grid-cols-3 gap-4 ">
+      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
         <div>
           <h2 className=" text-2xl font-bold "> Visit Us: </h2>
           <p>3100 Clarendon Blvd #200</p>
--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,29 +1,38 @@
 import Image from "next/image";
-import BANNER_ING from "../../assets/banner_img1.webp";
+import { CgArrowTopRight } from "react-icons/cg";
+import BANNER_ING from "../../assets/banner_img/image_2.webp";
 
 export default function Banner() {
   return (
     <>
-      <div className="container flex flex-col-reverse md:flex-row lg:flex-row p-4 space-y-4 md:space-y-0 md:space-x-4">
+      <div className="container flex flex-col-reverse lg:flex-row lg:items-center">
         <div className="flex-1">
           <h1 className="text-8xl text-primary font-bold mt-8 mb-20">
             Welcome To Quantum
           </h1>
-          <p className="mb-12">
+          <p className="font-semibold text-xl mb-12">
             Discover an affordable, fast-track path to professional success at
             our accelerated higher-learning institution.
           </p>
-          <p>
+          <p className="font-semibold text-xl">
             Experience the fusion of academia and industry firsthand. Secure
-            your spot on our waiting list for the Summer Semester of 2024.
+            your spot on our waiting list for the Summer Semester of {"2024"}.
           </p>
           <div className="flex items-center gap-4 my-8">
             <button className="customButton">Apply Today</button>
-            <button className="customButton">Learn More -- </button>
+            <button className="customButton flex items-center">
+              Learn More <CgArrowTopRight />{" "}
+            </button>
           </div>
         </div>
-        <div className="flex-1 lg:relative">
-          <Image src={BANNER_ING} alt="banner_img" fill={true} />
+
+        <div className="flex-1 lg:relative mt-8  ">
+          <Image
+            src={BANNER_ING}
+            alt="banner_img"
+            className="w-full h-full lg:h-[700px] object-cover"
+            sizes="80vw"
+          />
         </div>
       </div>
     </>
