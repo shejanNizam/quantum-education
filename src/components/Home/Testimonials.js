85a63f68d911b327c58dfe1c9b4955eaeb20e49f
nizam <nizam.shejan11@gmail.com>
2024-12-22 17:26:17 +0600

    refactor home page

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -52,7 +52,6 @@ export default function Testimonials() {
       </h2>
 
       <Swiper
-        // pagination={{ type: "bullets", clickable: true }}
         autoplay={true}
         loop={true}
         navigation={true}
