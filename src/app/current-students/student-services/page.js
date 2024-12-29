141f8fe531fedd29970dcd26fcda442b596b89e0
nizam <nizam.shejan11@gmail.com>
2024-12-29 16:44:15 +0600

    contact section reafctor

--- src/app/current-students/student-services/page.js
+++ src/app/current-students/student-services/page.js
@@ -9,6 +9,7 @@ import {
 } from "react-icons/fa";
 
 import { getMetadata } from "@/lib/utils";
+import ContactUsComp from "@/lib/ContactUsComp";
 export async function generateMetadata() {
   return getMetadata({ title: "StudentServices - Quantum Institute" });
 }
@@ -102,40 +103,7 @@ export default function StudentServicesPage() {
           </div>
         </div>
         {/* contact */}
-        <div className="bg-gray-50 p-8">
-          <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-            {/* Left Section */}
-            <div>
-              <h4 className="text-3xl font-bold text-gray-800 mb-4">
-                Contact us
-              </h4>
-              <p className="text-gray-600 leading-relaxed mb-4">
-                If you have questions or require more information about our
-                programs or potential future graduate offerings, please don’t
-                hesitate to reach out to us at{" "}
-                <span className="font-bold text-gray-800">
-                  info@qust.education
-                </span>
-                . Our team is here to assist you and provide the guidance you
-                need to make the best educational choices.
-              </p>
-              <p className="text-gray-600 leading-relaxed">
-                Quantum is dedicated to your educational success and growth. We
-                appreciate your interest in our institution and look forward to
-                supporting you on your educational journey, whether through our
-                existing programs or future possibilities.
-              </p>
-            </div>
-
-            {/* Right Section */}
-            <div className="flex flex-col items-center justify-center text-center">
-              <FaEnvelope className="text-6xl text-primary mb-4" />
-              <p className="text-2xl font-bold text-gray-800">
-                info@qust.education
-              </p>
-            </div>
-          </div>
-        </div>
+        <ContactUsComp heading={"Contact Us"} />
 
         {/*  */}
         <p>
--- src/lib/AcademicGraUndCalComp.js
+++ src/lib/AcademicGraUndCalComp.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FaEnvelope } from "react-icons/fa";
+import ContactUsComp from "./ContactUsComp";
 
 export default function AcademicGraUndCalComp({
   subHeading,
@@ -50,40 +50,7 @@ export default function AcademicGraUndCalComp({
         </div>
 
         {/* contact us start */}
-        <div className="bg-gray-50 p-8">
-          <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-            {/* Left Section */}
-            <div>
-              <h4 className="text-3xl font-bold text-gray-800 mb-4">
-                Contact us
-              </h4>
-              <p className="text-gray-600 leading-relaxed mb-4">
-                If you have questions or require more information about our
-                programs or potential future graduate offerings, please don’t
-                hesitate to reach out to us at{" "}
-                <span className="font-bold text-gray-800">
-                  info@qust.education
-                </span>
-                . Our team is here to assist you and provide the guidance you
-                need to make the best educational choices.
-              </p>
-              <p className="text-gray-600 leading-relaxed">
-                Quantum is dedicated to your educational success and growth. We
-                appreciate your interest in our institution and look forward to
-                supporting you on your educational journey, whether through our
-                existing programs or future possibilities.
-              </p>
-            </div>
-
-            {/* Right Section */}
-            <div className="flex flex-col items-center justify-center text-center">
-              <FaEnvelope className="text-6xl text-primary mb-4" />
-              <p className="text-2xl font-bold text-gray-800">
-                info@qust.education
-              </p>
-            </div>
-          </div>
-        </div>
+        <ContactUsComp heading={"Contact Us"} />
       </div>
     </>
   );
